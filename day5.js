7d4d480d920afe60317a2a24ad021d9ee6daf28e
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-04-22 23:14:11 +0530

    data types and Stack and Heap

---
+++ day5.js
@@ -0,0 +1,10 @@
+///## primitive data types-
+///string,Null,boolean,Number,Undefined,symbol,Bigint
+
+
+//******************************************************************* */
+
+//HOW MEMORY WORKS IN JAVASCRIPT
+
+// primitive type data uses stack
+//Non primitive data uses Heap
