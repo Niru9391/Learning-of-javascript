0f3d846a70c401078fbca9d0f513a5d9b4a1ae59
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-04-22 22:59:47 +0530

    Learning of datatypes conversion

---
+++ day4.js
@@ -0,0 +1,11 @@
+//DATA TYPES CAMPARISONS
+
+//    console.log(4>5);      //OUTPUT    -    FALSE
+ //  console.log(2=="2");    //True
+  ///console.log(2==="2")    //False
+console.log(null>0);//False
+console.log(null>=0);//true
+console.log(null==0);//false
+
+///=== Strict check-it checks the datatype and number
+
