7e3147c05734cb18acbbeeb342bf7f672343ecc0
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-04-23 21:16:36 +0530

    play with number

---
+++ day7.js
@@ -0,0 +1,29 @@
+//Number and Math
+
+
+/*
+const score  = 6
+
+console.log(score)
+const balance = new Number(9)
+console.log(balance)
+console.log(isFinite(balance))
+console.log(balance.toString().length)
+console.log(balance.toFixed(3))
+const balance1 = 23.8976
+console.log(balance1.toPrecision(4))
+const hundred = 10000000
+console.log(hundred.toLocaleString())
+*/
+/// *************************************MATH*****************************************
+console.log(Math)//datatype object
+console.log(Math.abs(-3))//negative convert to positive
+console.log(Math.round(4.3))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.3))
+console.log(Math.floor(4.3))
+console.log(Math.min(1,2,-4,3))
+console.log(Math.random())//Value between 0 to 1
+const min = 10
+const max= 90
+console.log(Math.floor(Math.random()*(max-min+1)+min))
