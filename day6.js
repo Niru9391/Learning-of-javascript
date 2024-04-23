1562db6c77ad57a54f1409d98a0d34fe80c2344e
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-04-23 20:48:57 +0530

    string operation

---
+++ day6.js
@@ -0,0 +1,16 @@
+/*
+const name ='Niru'
+const num=67
+console.log(name + num);
+
+console.log(`Hello My name is ${name}`)
+*/
+//ASSIGNING STRING NEW WAY
+//String is an object
+const game = new String(" NIRUN ")
+console.log(game[0])
+console.log(game.toLowerCase())
+console.log(game.charAt(3))
+console.log(game.indexOf('N'))
+console.log(game.slice(0,-2))
+console.log(game.trim())//tRIM white space
