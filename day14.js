da6f363d4dc6ef379d4f879e7fcd0abaaf5474b1
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-04-29 23:47:59 +0530

    control flow

---
+++ day14.js
@@ -0,0 +1,112 @@
+//  Control Flow
+//1.if
+if(true){
+//code will execute if condition are true
+}
+//var is not scope variable
+/*
+const score =200
+if(score>100){
+ const power ="fly"
+    console.log(`user power ${power}`)
+}
+console.log(`user power ${power}`)
+*/
+//Switch
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+/*
+const month = 121;
+switch (month) {
+  
+
+
+    case 1:
+        console.log("January")
+        
+        break;
+  
+
+   
+    case 2:
+        console.log("feb")
+        
+        break;
+    case 3:
+        console.log("March")
+        
+        break;
+    case 4:
+        console.log("April")
+        
+        break;
+    case 5:
+        console.log("May")
+        
+        break;
+    case 6:
+        console.log("June")
+        
+        break;
+    case 7:
+        console.log("July")
+        
+        break;
+    case 8:
+        console.log("August")
+        
+        break;
+    case 9:
+        console.log("Sept")
+        
+        break;
+    case 10:
+        console.log("Octo")
+        
+        break;
+    case 11:
+        console.log("Nov")
+        
+        break;
+    case 12:
+        console.log("Decem")
+        
+        break;
+
+    default:
+        console.log("Not a month")
+        break;
+
+       
+}
+*/
+//TRUTHY AND FALSY
+/*
+const usermail ="heu"
+if(usermail){
+    console.log("got mail")
+}
+else{
+    console.log("don't have mail")
+}
+*/
+//Falsy value-:false,0,-0,Bigint(0n),null,undefined,Nan
+//Truthy value-:"0",'false'," ",[],{},function(){}
+//Nullish coalescing operator(??):null undefined::null undefined
+/*
+let val1;
+val1=null??10
+console.log(val1)
+*/
+//Ternary operator
+
+//conditon?true:false
+console.log(2==2?true:false)
