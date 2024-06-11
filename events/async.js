ee0bea965330fa8aa547850cfd8d6281a696ae68
Niranjan kumar <niranjanyadav9655@gmail.com>
2024-06-11 21:35:00 +0530

    async

---
+++ events/async.js
@@ -0,0 +1,8 @@
+// By defautl,
+// js is asynchronous.
+// js is single threaded.
+//***********        Execution context      ***************** */
+//Each operation excute when last operation completed.
+//     BLOCKING CODE VS NON BLOCKING CODE
+// blocking code block all the flow of code.(Read file sync)
+//non blocking code does  not block the flow of code.(Read file Async)
